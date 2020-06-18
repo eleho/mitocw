9d0304a9a10f864239296c54ed44e1ba5ce120b6
eleho <ed.eleho@gmail.com>
2020-06-18 07:42:05 -0500

    Modified inputs to become int

--- ps0.py
+++ ps0.py
@@ -1,7 +1,7 @@
 import numpy
 
-var1 = input("Enter number x: ")
-var2 = input("Enter number y: ")
+var1 = int(input("Enter number x: "))
+var2 = int(input("Enter number y: "))
 
 sol1 = var1**var2
 sol2 = int(numpy.log2(var1))
